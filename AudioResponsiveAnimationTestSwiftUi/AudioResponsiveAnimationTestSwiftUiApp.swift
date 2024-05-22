37bed93e2137bbbad9fbaca07d86876732267a87
Matthew Goncalves Da Silva <matthewgdasilva25@gmail.com>
2024-05-22 10:17:04 +0100

    Initial Commit

---
+++ AudioResponsiveAnimationTestSwiftUi/AudioResponsiveAnimationTestSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioResponsiveAnimationTestSwiftUiApp.swift
+//  AudioResponsiveAnimationTestSwiftUi
+//
+//  Created by Matt Da Silva on 22/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct AudioResponsiveAnimationTestSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AudioResponsiveAnimationTestSwiftUi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AudioResponsiveAnimationTestSwiftUi
+//
+//  Created by Matt Da Silva on 22/05/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
